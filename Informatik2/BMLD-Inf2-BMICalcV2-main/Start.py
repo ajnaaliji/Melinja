a6399aa09175a85ecf2a62f0f56714dcfaa30869
MeliAjna <alijiajn@students.zhaw.ch>
2025-04-03 18:11:40 +0200

    neu

---
+++ Informatik2/BMLD-Inf2-BMICalcV2-main/Start.py
@@ -0,0 +1,35 @@
+# === Initialize the data manager ===
+import pandas as pd
+from utils.data_manager import DataManager
+
+# initialize the data manager
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_DB")  # switch drive 
+
+# load the data from the persistent storage into the session state
+data_manager.load_user_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value = pd.DataFrame(), 
+    parse_dates = ['timestamp']
+    )
+
+# === Initialize the login manager ===
+from utils.login_manager import LoginManager
+
+login_manager = LoginManager(data_manager) # initialize login manager
+login_manager.login_register()  # opens login page
+
+# === Start with actual app ===
+import streamlit as st
+
+st.title('BMI Rechner')
+
+st.markdown(f"✨ Hallo! ✨")
+st.markdown("🏃 Die Anwendung ermöglicht es Ihnen, Ihren BMI zu berechnen und im Zeitverlauf zu verfolgen 📊")
+        
+# Add some health advice
+st.info("""Der BMI ist ein Screening-Tool, aber keine Diagnose für Körperfett oder Gesundheit. 
+Bitte konsultieren Sie einen Arzt für eine vollständige Beurteilung.""")
+
+st.write("Diese App wurde von Samuel Wehrli im Rahmen des Moduls 'BMLD Informatik 2' an der ZHAW entwickelt.")
+
